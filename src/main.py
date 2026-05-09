3f59c194ed1b35fbeffb62b10c86f5eced64fc28
Solganis <solganis.dev@gmail.com>
2026-05-09 22:49:18 +0300

    fix(build): hide window during startup in built mode

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import flet as ft
 
-from steamcleaner.ui.gui.app import SteamCleanerGUI
+from steamcleaner.ui.gui.app import SteamCleanerGUI, _WindowHider
+
+hider = _WindowHider()
+hider.start()
 
 
 async def main(page: ft.Page):
-    gui = SteamCleanerGUI(page)
+    gui = SteamCleanerGUI(page, hider)
     await gui.initialize()
 
 
